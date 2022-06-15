17c92043dc401e3d73c141de8f1ae0ec4a56d1b5
goodwyyn <godwin.mtuma@student.moringaschool.com>
2022-06-15 16:06:22 +0300

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+// original
+input.addEventListener('click', function() {
+    alert('I was clicked!');
+
+})
+
+//binds an event listener in addingEventListener()
+
+function addingEventListener (){
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    
+    })
+    
 }
+
+console.log ('addingEventListener')
